0e434ea111b3c44c05ca5f7f003f9870f384aa36
root <root@localhost.localdomain>
2019-04-10 13:10:39 +0300

    Created skeleton

--- BashTerminal.sh
+++ BashTerminal.sh
@@ -1,1 +1,21 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+clear
+
+Menu(){
+  echo -e "1. Packet manager\n2. User manager\n0. Exit";
+  read choice
+}
+
+
+exit=true
+
+while [ $exit == true ]
+do
+  Menu;
+  case $choice in
+    1) source lib/PacketManager.sh; ;;
+    2) source lib/UserManager.sh; ;;
+    0) echo "Bye!"; let exit=false; ;;
+    *) echo "Wring choice!"; ;;
+  esac
+done
---
+++ lib/PacketManager.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+clear
+echo "Packet Manager";
